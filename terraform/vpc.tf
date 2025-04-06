f61b5ce9e4a5caa7997f696345b2dcddbe940db8
nishantnesargi13 <156416844+nishantnesargi13@users.noreply.github.com>
2025-04-06 08:47:24 +0530

    Create vpc.tf

---
+++ terraform/vpc.tf
@@ -0,0 +1,17 @@
+resource "aws_security_group" "eks_security_group" {
+  vpc_id = aws_vpc.eks_vpc.id
+
+  egress {
+    cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+  }
+
+  ingress {
+    cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+  }
+}
